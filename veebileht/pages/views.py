cc964a626c7e8f4a4445ea850fbdba3b4546d4e5
Veiko Mäesalu <veiko.maesalu@gmail.com>
2022-09-16 22:41:59 +0300

    form seaded ja esmane töö sellega tundub toimivat

--- veebileht/pages/views.py
+++ veebileht/pages/views.py
@@ -4,5 +4,27 @@ def home(request):
 	return render(request, "home.html", {})
 
 def add(request):
-	return render(request, "add.html", {})
+	from random import randint
+	num1 = randint(0,10)
+	num2 = randint(0,10)
+
+	if request.method == "POST":
+		answer = request.POST['vastus']
+		vana_num_1 = request.POST['vana_num1']
+		vana_num_2 = request.POST['vana_num2']
+
+		correct_answer = int(vana_num_1) + int(vana_num_2)
+		if correct_answer == int(answer):
+			minu_vastus = "ÕIGE!"
+		else:
+			minu_vastus = "VALE!"
+		return render(request, "add.html", 
+		{'minuvastus':minu_vastus,
+		'num_1':num1,
+		'num_2':num2 })
+
+
+	return render(request, "add.html",
+	{'num_1':num1,
+	'num_2':num2 })
 
