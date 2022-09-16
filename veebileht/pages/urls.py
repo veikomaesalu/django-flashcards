35cf2bb8d9b5da51b138c3d563be9f72b8b14dba
Veiko Mäesalu <veiko.maesalu@gmail.com>
2022-09-16 21:26:44 +0300

    commit peale põhiseadete tegemist, peaks töötama, kuigi millegipärast eelmine kord ei töötanud.

--- veebileht/pages/urls.py
+++ veebileht/pages/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.home, name="home"),
-    path("add.html", views.add, name="add"),
+    path("add/", views.add, name="add"),
 ]
