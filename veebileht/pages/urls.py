c9a829398e8dab82d733852a92ccc088f0f644a7
Veiko Mäesalu <veiko.maesalu@gmail.com>
2022-09-16 21:33:38 +0300

    Mingi commit

--- veebileht/pages/urls.py
+++ veebileht/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("add/", views.add, name="add"),
 ]
