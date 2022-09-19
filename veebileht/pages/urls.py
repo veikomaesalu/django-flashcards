6d5e41434c5da4c81e2fd87112f32d96a17b4c70
Veiko Mäesalu <veiko.maesalu@gmail.com>
2022-09-19 11:13:36 +0300

    lahutamise osa kuni värvilise messagebox-ni(mis sai kleebitudaga seadmata)

--- veebileht/pages/urls.py
+++ veebileht/pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("add/", views.add, name="add"),
+    path("subst/", views.subst, name="subst"),
 ]
--- veebileht/pages/views.py
+++ veebileht/pages/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 def home(request):
 	return render(request, "home.html", {})
 
+
+
+
+
+
 def add(request):
 	from random import randint
 	num1 = randint(0,10)
@@ -28,3 +33,26 @@ def add(request):
 	{'num_1':num1,
 	'num_2':num2 })
 
+def subst(request):
+	from random import randint
+	arv1 = randint(0,10)
+	arv2 = randint(0,10)
+
+	if request.method == "POST":
+		vastus_tagasi = request.POST['vastus']
+		esimene_nr_tagasi = request.POST['esimene_arv']
+		teine_nr_tagasi = request.POST['teine_arv']
+		oige_vastus = int(esimene_nr_tagasi) - int(teine_nr_tagasi)
+		if oige_vastus == int(vastus_tagasi):
+			minu_vastus = "Õigus!!! " + esimene_nr_tagasi + ' - ' + teine_nr_tagasi + ' on ' + str(vastus_tagasi)
+		else:
+			minu_vastus = "Vale!!!  " + esimene_nr_tagasi + ' - ' + teine_nr_tagasi + ' ei ole ' + str(vastus_tagasi)
+		return render(request, "subst.html", 
+			{'vastus_tagasi':vastus_tagasi,
+			 'minu_vastus':minu_vastus,
+			 'arv1':arv1,
+		 	 'arv2':arv2})
+
+	return render(request, "subst.html", 
+		{'arv1':arv1,
+		 'arv2':arv2})
